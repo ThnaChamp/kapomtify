ab22318e6c7424e177872807131ddb8a34a03a27
softkoi <Kawinpopjularee@gmail.com>
2026-05-07 12:45:18 +0700

    test

--- server/src/app.js
+++ server/src/app.js
@@ -24,5 +24,10 @@ app.use('/api/artists', artistRoutes);
 app.use('/api/genres', genreRoutes);
 
 app.listen(PORT, () => {
+    console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+    console.log('DB_HOST:', process.env.DB_HOST);
+    console.log('DB_PORT:', process.env.DB_PORT);
+    console.log('DB_NAME:', process.env.DB_NAME);
     console.log(`Server is running on port ${PORT}`);
 });
