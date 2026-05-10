2153b60a2af04da028923e9ee96dd5565e278910
COPTER <copter0905230050@gmail.com>
2026-05-10 19:57:04 +0700

    add detail PDF and REFUND

--- server/src/routes/transactionRoutes.js
+++ server/src/routes/transactionRoutes.js
@@ -2,13 +2,11 @@ const express = require("express");
 const router = express.Router();
 const transactionController = require("../controllers/transactionController");
 
-// GET /api/transactions?search=&status=&page=
 router.get("/", transactionController.getAllTransactions);
-
-// GET /api/transactions/:id
+router.get("/:id/detail", transactionController.getTransactionDetail);
+router.get("/:id/invoice", transactionController.getInvoicePDF);
 router.get("/:id", transactionController.getTransactionById);
-
-// DELETE /api/transactions/:id
+router.patch("/:id/refund", transactionController.refundTransaction);
 router.delete("/:id", transactionController.deleteTransaction);
 
 module.exports = router;
